857a18eb07c2e59ac93027c78f6584df8174a089
Gleb <glsidorenko01@gmail.com>
2024-12-04 16:26:29 +0300

    refactor: make code cleaner

--- src/app/pages/catalogue/catalogue.component.ts
+++ src/app/pages/catalogue/catalogue.component.ts
@@ -4,9 +4,10 @@ import { CardComponent } from '../../ui/card/card.component';
 import { NumberToArrayPipe } from "../../number-to-array.pipe";
 import { Product } from '../../types';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, filter, OperatorFunction, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, tap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { CartService } from '../../services/cart/cart.service';
+import { coerceProductPrice, filterNilValue } from '../../core-operators/operators';
 
 @Component({
   selector: 'app-catalogue',
@@ -21,24 +22,20 @@ export class CatalogueComponent {
   products: WritableSignal<Product[]> = signal(products);
   itemsPerPage: WritableSignal<number> = signal(10);
   currentPage: WritableSignal<number> = signal(1);
-  isProductFound: WritableSignal<boolean> = signal(false);
   isSearched: WritableSignal<boolean> = signal(false);
-  isSorted: WritableSignal<boolean> = signal(false);
   searchedItems: WritableSignal<Product[]> = signal([]);
-
   searchProduct$  = new FormControl<string>('');
-  selectedValue$ = new FormControl<string>({value: '', disabled: false}, Validators.required);
+  selectedValue$ = new FormControl<string>({value: 'default', disabled: false}, Validators.required); // сделать enable effect sortingstagechangeeffect
   productsPerPage$ = new FormControl<number>(10);
 
   totalCount: Signal<number> = computed(() => {
     return this.searchedItems().length > 0 ? this.searchedItems().length : this.products().length;
   })
 
-  visibleItems: Signal<Product[] | undefined> = computed(() => {
+  visibleItems: Signal<Product[]> = computed(() => {
     const items = this.products();
     const perPage = this.itemsPerPage();
     const page = this.currentPage();
-    const isFound = this.isProductFound();
     const searchedItems = this.searchedItems();
     const isSearched = this.isSearched();
 
@@ -46,20 +43,16 @@ export class CatalogueComponent {
       return array.slice((page - 1) * perPage, perPage * page);
     }
 
-    while (searchedItems.length !== 0) {
+    while (searchedItems.length > 0) {
       return transformArray(searchedItems);
     }
 
-    if(!isFound && isSearched) {
-      return undefined;
+    if (searchedItems.length === 0 && isSearched) {
+      return [];
     }
 
     return transformArray(items);
   })
-
-  itemsArrayToShow() {
-    return this.isSearched() ? this.searchedItems() : this.products();
-  }
 
   constructor(@Inject(PLATFORM_ID) public platformId: string) {
     this.searchProduct$.valueChanges.pipe(
@@ -69,17 +62,13 @@ export class CatalogueComponent {
         tap(search => {
           this.selectedValue$.enable();
           this.isSearched.set(true);
-          this.isProductFound.set(true);
           this.searchedItems.set(this.products().filter(item => item.title.toLowerCase().includes(search.toLowerCase())))
-        }),
-        tap(search => {
+
           if (!search) { // пустой поиск
             this.isSearched.set(false);
             this.searchedItems.set([]);
-            this.isProductFound.set(false);
           } 
           if (search && !this.searchedItems().length) { // если не нашло
-            this.isProductFound.set(false);
             this.selectedValue$.disable();
           }
         }),
@@ -91,23 +80,24 @@ export class CatalogueComponent {
       debounceTime(300),
       distinctUntilChanged(),
       tap((value) => {
-        this.isSorted.set(true);
+        const arr = this.searchedItems().length > 0 ? this.searchedItems() : this.products();
+        
         switch(value) {
           case 'sale' : {
-            return this.searchedItems.set((this.itemsArrayToShow() as Product[]).filter((value) => value.salePrice))
+            return this.searchedItems.set(arr.filter((value) => value.salePrice))
           }
           case 'sortAsc' : {
-            return this.searchedItems.set([...(this.itemsArrayToShow() as Product[])].sort((a, b) => {
-              const firstPrice = Object.hasOwn(a, 'salePrice') && a.salePrice !== null ? a.salePrice : a.price 
-              const secondPrice = Object.hasOwn(b, 'salePrice') && b.salePrice !== null ? b.salePrice : b.price 
-              return (firstPrice as number) - (secondPrice as number);
+            return this.searchedItems.set([...arr].sort((a, b) => {
+              const firstPrice = coerceProductPrice(a)
+              const secondPrice = coerceProductPrice(b)
+              return firstPrice - secondPrice;
             }))
           }
           case 'sortDecs' : {
-            return this.searchedItems.set([...(this.itemsArrayToShow() as Product[])].sort((a, b) => {
-              const firstPrice = Object.hasOwn(a, 'salePrice') && a.salePrice !== null ? a.salePrice : a.price 
-              const secondPrice = Object.hasOwn(b, 'salePrice') && b.salePrice !== null ? b.salePrice : b.price
-              return (secondPrice as number) - (firstPrice as number)
+            return this.searchedItems.set([...arr].sort((a, b) => {
+              const firstPrice = coerceProductPrice(a)
+              const secondPrice = coerceProductPrice(b)
+              return secondPrice - firstPrice
             }))
           }
         }
@@ -126,9 +116,7 @@ export class CatalogueComponent {
     effect(() => console.log({
       visibleItems: this.visibleItems(),
       searchedItems: this.searchedItems(),
-      isProductFound: this.isProductFound(),
       isSearched: this.isSearched(),
-      isSorted: this.isSorted()
     }));
   }
 
@@ -173,6 +161,3 @@ export class CatalogueComponent {
   }
 }
 
-export function filterNilValue<T>(): OperatorFunction<T, NonNullable<T>> {
-  return filter((value: T): value is NonNullable<T> => value !== null && value !== undefined);
-}

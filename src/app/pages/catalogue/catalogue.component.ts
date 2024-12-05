71c3e50856173ac9786ff79f5eaec262adb1fa62
AlexProffe <alexweb-dev@mail.ru>
2024-12-05 15:57:22 +0300

    Refactor catalog and pagination components

--- src/app/pages/catalogue/catalogue.component.ts
+++ src/app/pages/catalogue/catalogue.component.ts
@@ -1,161 +1,115 @@
-import { Component, computed, Inject, PLATFORM_ID, Signal, signal, WritableSignal, effect, inject } from '@angular/core';
+import {
+  Component,
+  computed,
+  signal,
+  WritableSignal,
+  effect,
+  inject
+} from '@angular/core';
 import { products } from '../../products';
 import { CardComponent } from '../../ui/card/card.component';
-import { NumberToArrayPipe } from "../../number-to-array.pipe";
+import { NumberToArrayPipe } from '../../number-to-array.pipe';
 import { Product } from '../../types';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { combineLatest, debounceTime, distinctUntilChanged, tap } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { CartService } from '../../services/cart/cart.service';
 import { coerceProductPrice, filterNilValue } from '../../core-operators/operators';
+import { PaginationComponent } from '../../ui/pagination/pagination.component';
 
 @Component({
   selector: 'app-catalogue',
   standalone: true,
-  imports: [CardComponent, NumberToArrayPipe, ReactiveFormsModule],
-  templateUrl: "./catalogue.component.html",
+  imports: [CardComponent, NumberToArrayPipe, ReactiveFormsModule, PaginationComponent],
+  templateUrl: './catalogue.component.html',
   styleUrl: './catalogue.component.css'
 })
 
 export class CatalogueComponent {
-  cartService = inject(CartService);
+  // cartService = inject(CartService);
+
   products: WritableSignal<Product[]> = signal(products);
-  itemsPerPage: WritableSignal<number> = signal(10);
-  currentPage: WritableSignal<number> = signal(1);
-  isSearched: WritableSignal<boolean> = signal(false);
-  searchedItems: WritableSignal<Product[]> = signal([]);
-  searchProduct$  = new FormControl<string>('');
-  selectedValue$ = new FormControl<string>({value: 'default', disabled: false}, Validators.required); // сделать enable effect sortingStageChangeEffect
-  productsPerPage$ = new FormControl<number>(10);
 
-  srx = combineLatest([this.searchProduct$.valueChanges, this.selectedValue$.valueChanges]).subscribe(console.log)
+  searchProduct = new FormControl<string>('');
 
-  totalCount: Signal<number> = computed(() => {
-    return this.searchedItems().length > 0 ? this.searchedItems().length : this.products().length;
+  selectedValue = new FormControl<string>({value: 'default', disabled: false}, {
+    nonNullable: true,
+    validators: [Validators.required]
+  }); // сделать enable effect sortingStageChangeEffect
+
+  productsPerPage = new FormControl<number>(10, {nonNullable: true});
+
+  protected readonly sortingStateChangeEffect = effect(() => {
+    const totalCount = this.searchedItems().length;
+
+    if(!totalCount) {
+      this.selectedValue.disable();
+
+      return;
+    }
+
+    this.selectedValue.enable();
   })
 
-  visibleItems: Signal<Product[]> = computed(() => {
-    const items = this.products();
-    const perPage = this.itemsPerPage();
-    const page = this.currentPage();
-    const searchedItems = this.searchedItems();
-    const isSearched = this.isSearched();
-
-    function transformArray(array: Product[]) {
-      return array.slice((page - 1) * perPage, perPage * page);
-    }
-
-    while (searchedItems.length > 0) {
-      return transformArray(searchedItems);
-    }
-
-    if (searchedItems.length === 0 && isSearched) {
-      return [];
-    }
-
-    return transformArray(items);
-  })
-
-  constructor(@Inject(PLATFORM_ID) public platformId: string) {
-    this.searchProduct$.valueChanges.pipe(
-        filterNilValue(),
-        debounceTime(300),
-        distinctUntilChanged(),
-        tap(search => {
-
-          const arr = this.searchedItems().length > 0 ? this.searchedItems() : this.products();
-          const x  = this.products();
-
-          this.selectedValue$.enable();
-          this.isSearched.set(true);
-          this.searchedItems.set(x.filter(item => item.title.toLowerCase().includes(search.toLowerCase())))
-
-          if (!search) { // пустой поиск
-            this.isSearched.set(false);
-            this.searchedItems.set([]);
-          } 
-          // if (search && !this.searchedItems().length) { // если не нашло
-          //   this.selectedValue$.disable();
-          // }
-        }),
-        takeUntilDestroyed(),
-    ).subscribe();
-
-    this.selectedValue$.valueChanges.pipe(
+  searchValueChangeSignal = toSignal(
+    this.searchProduct.valueChanges.pipe(
       filterNilValue(),
       debounceTime(300),
       distinctUntilChanged(),
-      tap((value) => {
-        const arr = this.searchedItems().length > 0 ? this.searchedItems() : this.products();
-        
-        switch(value) {
-          case 'sale' : {
-            return this.searchedItems.set(arr.filter((value) => value.salePrice))
-          }
-          case 'sortAsc' : {
-            return this.searchedItems.set([...arr].sort((a, b) => coerceProductPrice(a) - coerceProductPrice(b)))
-          }
-          case 'sortDecs' : {
-            return this.searchedItems.set([...arr].sort((a, b) => coerceProductPrice(b) - coerceProductPrice(a)))
-          }
-        }
-      }),
-      takeUntilDestroyed()
-    ).subscribe()
+    )
+  );
 
-    this.productsPerPage$.valueChanges.pipe(
+  sortValueChangeSignal = toSignal(
+    this.selectedValue.valueChanges.pipe(
       filterNilValue(),
-      tap(value => {
-        this.itemsPerPage.set(value)
-      }),
-      takeUntilDestroyed()
-    ).subscribe()
+      debounceTime(300),
+      distinctUntilChanged(),
+    ), {initialValue: 'default'}
+  );
 
-    // effect(() => console.log({
-    //   visibleItems: this.visibleItems(),
-    //   searchedItems: this.searchedItems(),
-    //   isSearched: this.isSearched(),
-    // }));
-  }
+  searchedItems = computed(() => {
+    const search = this.searchValueChangeSignal();
+    const items = this.products();
+    const sort = this.sortValueChangeSignal();
 
-  pageCount: Signal<number> = computed(() => {
-    const count = this.itemsPerPage();
-    const total = this.totalCount();
-    return Math.ceil(total / count);
-  })
+    const filteredItems = items.filter(item => {
+      if (!search) {
+        return true;
+      }
 
-  nextPage(): void {
-    const isLastPage = this.isLastPage();
-    if (isLastPage) {
-      return
+      return item.title.toLowerCase().includes(search.toLowerCase());
+    });
+
+    return this.processSortCondition(filteredItems, sort, 'salePrice');
+  });
+
+  private processSortCondition<T extends Product>(items: T[], sort: string, saleKey: keyof T): T[] {
+    switch (sort) {
+      case 'sale' : {
+        return items.filter((value) => !!value[saleKey]);
+      }
+      case 'sortAsc' : {
+        return items.sort((a, b) => coerceProductPrice(a) - coerceProductPrice(b));
+      }
+
+      case 'sortDecs' : {
+        return items.sort((a, b) => coerceProductPrice(b) - coerceProductPrice(a));
+      }
+
+      case 'sortNameAsc': {
+        return items.sort((a, b) => a.title.localeCompare(b.title));
+      }
+
+      case 'sortNameDesc': {
+        return items.sort((a, b) => b.title.localeCompare(a.title));
+      }
+
+      default: {
+        return items;
+      }
     }
-    this.currentPage.update((val: number) => val + 1);
   }
 
-  previousPage(): void {
-    const isFirstPage = this.isFirstPage();
-
-    if (isFirstPage) {
-      return
-    }
-
-    this.currentPage.update((val: number) => val - 1);
-  }
-
-  isFirstPage: Signal<boolean> = computed(() => {
-    const currentPage = this.currentPage();
-    return currentPage === 1;
-  })
-
-  isLastPage: Signal<boolean> = computed(() => {
-    const pageCount = this.pageCount();
-    const currentPage = this.currentPage();
-
-    return pageCount === currentPage;
-  })
-
-  updateCurrentPage(page: number): void {
-    this.currentPage.set(page);
-  }
+  constructor() {}
 }
 
--- src/app/ui/pagination/pagination.component.ts
+++ src/app/ui/pagination/pagination.component.ts
@@ -1,14 +1,92 @@
-import { Component } from '@angular/core';
-
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input, InputSignal,
+  output,
+  signal,
+  Signal,
+  TemplateRef
+} from '@angular/core';
+import { NumberToArrayPipe } from '../../number-to-array.pipe';
+import { JsonPipe, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [],
+  imports: [
+    NumberToArrayPipe,
+    NgTemplateOutlet,
+    JsonPipe
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.css'
 })
-export class PaginationComponent{
+export class PaginationComponent<T = unknown> {
+  public readonly items: InputSignal<T[]> = input<T[]>([]);
 
-  
+  public readonly itemsPerPage: InputSignal<number> = input.required();
+
+  public readonly itemTemplate: InputSignal<TemplateRef<T> | undefined> = input();
+
+  public readonly pageChanged = output<number>();
+
+  public readonly currentPage = signal(1);
+
+  public readonly totalCount: Signal<number> = computed(() => this.items().length)
+
+  public readonly pageCount = computed(() => {
+    const count = this.itemsPerPage();
+    const total = this.totalCount();
+    return Math.ceil(total / count);
+  });
+
+  public readonly visibleItems = computed(() => {
+    const items = this.items();
+    const perPage = this.itemsPerPage();
+    const page = this.currentPage();
+
+    return items.slice((page - 1) * perPage, perPage * page);
+  });
+
+  public readonly isFirstPage: Signal<boolean> = computed(() => {
+    const currentPage = this.currentPage();
+    return currentPage === 1;
+  })
+
+  public readonly isLastPage: Signal<boolean> = computed(() => {
+    const pageCount = this.pageCount();
+    const currentPage = this.currentPage();
+
+    return pageCount === currentPage;
+  })
+
+  public nextPage(): void {
+    const isLastPage = this.isLastPage();
+    if (isLastPage) {
+      return
+    }
+    this.currentPage.update((val: number) => val + 1);
+
+    this.pageChanged.emit(this.currentPage());
+  }
+
+  public previousPage(): void {
+    const isFirstPage = this.isFirstPage();
+
+    if (isFirstPage) {
+      return
+    }
+
+    this.currentPage.update((val: number) => val - 1);
+
+    this.pageChanged.emit(this.currentPage());
+  }
+
+  public updateCurrentPage(page: number): void {
+    this.currentPage.set(page);
+
+    this.pageChanged.emit(page);
+  }
 }
